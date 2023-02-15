9b8e2f1630e22e2f0e130f03b81d11d2f864e41d
Peter <bondarenko.volsu@gmail.com>
2023-02-15 21:55:29 +0100

    style: test for getter of task attributes

--- tests/testthat/test-object_builder.R
+++ tests/testthat/test-object_builder.R
@@ -1,44 +1,28 @@
 test_that("Testing function of get-task-attribute", {
     gta <- get_task_attributes("title: Konjunktiv two")
-
-
     example <- c("Konjunktiv two")
     names(example) <- "title"
-
     expect_equal(gta, example)
 }
 )
 test_that("Testing function of get-task-attribute", {
     gta <- get_task_attributes("identifier: 090909")
-
-
     example <- c("090909")
     names(example) <- "identifier"
-
     expect_equal(gta, example)
 }
 )
-
 test_that("Testing function of get-task-attribute", {
     gta <- get_task_attributes("options: 1.4,4.3,5.6")
-
-
-    example <- c("1.4,4.3,5.6")
+    example <- c(1.4,4.3,5.6)
     names(example) <- "options"
-
     expect_equal(gta, example)
 }
 )
-
-
 test_that("Testing function of get-task-attribute 2", {
     gta <- get_task_attributes("way: 26")
-
-
     example <- c("26")
     names(example) <- "way"
-
     expect_equal(gta, example)
 }
 )
-
