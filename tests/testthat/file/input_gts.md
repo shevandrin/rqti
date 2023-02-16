f18b2f3d832edf5884a4a1e79371af6cf02e4116
Peter <bondarenko.volsu@gmail.com>
2023-02-16 10:29:19 +0100

    test:get_task_section issue #17

---
+++ tests/testthat/file/input_gts.md
@@ -0,0 +1,14 @@
+question
+==========
+In economics it is generallz believed that the main objective of
+a Public Sector Financial Companz like Bank is to:
+attributes
+==========
+type: singlechoice
+title: Economics
+answers
+======
+Employ more and more people
+Maximize total production
+Maximize total profits
+Sell the goods at subsidized cost
--- tests/testthat/test-object_builder.R
+++ tests/testthat/test-object_builder.R
@@ -3,26 +3,37 @@ test_that("Testing function of get-task-attribute", {
     example <- c("Konjunktiv two")
     names(example) <- "title"
     expect_equal(gta, example)
-}
-)
+})
 test_that("Testing function of get-task-attribute", {
     gta <- get_task_attributes("identifier: 090909")
     example <- c("090909")
     names(example) <- "identifier"
     expect_equal(gta, example)
-}
-)
+})
 test_that("Testing function of get-task-attribute", {
     gta <- get_task_attributes("options: 1.4,4.3,5.6")
     example <- c(1.4,4.3,5.6)
     names(example) <- "options"
     expect_equal(gta, example)
-}
-)
-test_that("Testing function of get-task-attribute 2", {
+})
+test_that("Testing function of get-task-attribute", {
     gta <- get_task_attributes("way: 26")
     example <- c("26")
     names(example) <- "way"
     expect_equal(gta, example)
-}
-)
+})
+test_that("Testing function of get_task_section", {
+    gts <- get_task_section(readLines("./tests/testthat/file/input_gts.md"), "question")
+        example <- unlist(strsplit("In economics it is generallz believed that the main objective of\na Public Sector Financial Companz like Bank is to:", "\n"))
+    expect_equal(gts, example)
+})
+test_that("Testing function of get_task_section", {
+    gts <- get_task_section(readLines("./tests/testthat/file/input_gts.md"), "attributes")
+    example <- unlist(strsplit("type: singlechoice\ntitle: Economics","\n"))
+    expect_equal(gts, example)
+})
+test_that("Testing function of get_task_section", {
+    gts <- get_task_section(readLines("./tests/testthat/file/input_gts.md"), "answers")
+    example <- unlist(strsplit("Employ more and more people\nMaximize total production\nMaximize total profits\nSell the goods at subsidized cost","\n"))
+    expect_equal(gts, example)
+})
