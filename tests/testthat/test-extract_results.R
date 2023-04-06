bfa08436bf144a90b96b05b08b207bd00dc1794c
Johannes Titz <johannes@titz.science>
2023-04-06 15:09:59 +0200

    test: extract_results, compare with opal output

---
+++ tests/testthat/test-extract_results.R
@@ -0,0 +1,38 @@
+d <- qti::extract_result_zip("file/test_result.zip")
+
+# general analysis
+
+mycor <- function(x, y) {
+  if (sd(x) == 0) return(NA)
+  if (sd(y) == 0) return(NA)
+  cor(x, y)
+}
+
+# missing this:
+#  filter(is_answser_given == TRUE) %>%
+
+db <- d %>%
+  dplyr::group_by(file_name) %>%
+  dplyr::mutate(sum_score = sum(candidate_score)) %>%
+  dplyr::group_by(question_id) %>%
+  # correct for part-whole
+  # todo: remove sd = 0 first
+dplyr::summarize(n = dplyr::n(),
+            m_duration = mean(duration),
+            difficulty = mean(candidate_score / max_scores) * 100,
+            r_it = round(mycor(candidate_score, sum_score - candidate_score),
+                         2))
+
+d_exp <- read.csv(test_path("file", "statistics.csv"))
+d_exp <- d_exp %>%
+    dplyr::select(Nr., Response_count, Time_taken:Correlation)
+
+test_that("difficulty is correct",
+          {expect_equal(db$difficulty, d_exp$Difficulty)})
+
+test_that("discriminability is correct",
+          {expect_equal(db$r_it, d_exp$Correlation)})
+
+# seems the time is truncated in OPAL
+test_that("time is correct",
+          {expect_equal(trunc(db$m_duration), d_exp$Time_taken)})
