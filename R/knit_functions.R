ba37ea7ccbe509f04a386ff5563aac290d19bc40
Johannes Titz <johannes@titz.science>
2023-06-09 15:36:26 +0200

    feat(render): use qtijs as rendering engine
    Modify knit functions to work with qti package instead of exam. Some refactoring was also done. rmd2qti now creates a test.

--- R/knit_functions.R
+++ R/knit_functions.R
@@ -1,66 +1,91 @@
-#' Generate a HTML representation of a RMD file
+#' Render an RMD file as qti xml with QTIJS
 #'
-#' Choosing a specific Rmd file the function generates the qti.xml for it
-#' through the functions of the package exams, after the XML is copied into the
-#' QTI Engine which transforms the qti.xml into HTML format. Finally, the HTML
-#' is displayed and the user can have a preview of the exercise or exam.
+#' Generates the qti xml file through the functions of the package exams. The
+#' xml is copied into the QTIJS folder which transforms the xml into HTML.
+#' Finally, the HTML is displayed and the user can have a preview of the
+#' exercise or exam.
+#'
+#' Requires a running QTIJS server, which can be started with start_server().
 #'
 #' @param input (the path to the input Rmd document)
-#' @param ... other arguments that are currently ignored
+#' @param ... required for passing arguments when knitting
 #'
 #' @examples
 #' # Customize knit function in the Rmd file using the following YAML setting
 #' # after the word knit:
-#' \dontrun{qti::knit_qti_html}
+#' \dontrun{qti::knit}
 #'
 #' @export
 knit_qti_html <- function(input, ...) {
-  qti_port <- Sys.getenv("QTI_PORT")
-  url_engine <- paste("http://localhost", qti_port, sep = ":")
-  if (RCurl::url.exists(url_engine) == TRUE) {
-    qti_engine_d <- Sys.getenv("QTI_FOLDER")
-    files_d <- paste(qti_engine_d, "*.xml", sep = "/")
-    unlink(files_d)
-    exams::exams2qti21(input, dir = qti_engine_d, zip = FALSE)
-    rstudioapi::viewer(url_engine)
-  } else {
-    print(paste("QTI Engine not available in ",
-                url_engine,
-                ". Start the server with function start_server()"))
-  }
+    # naming this function just knit, does not work
+    # clean up
+    unlink(paste(qtijs_path(), "*.xml", sep = "/"))
+    url <- Sys.getenv("QTI_URL")
+    print(url)
+    if (url == "") {
+      stop("Server for QTIJS not running. You can start it manually with start_server()")
+    }
+    rmd2qti(input, dir = qtijs_path())
+    rstudioapi::viewer(url)
 }
 
-#' Start QTI Engine in a server
+#' Render a single xml file
 #'
-#' The function starts a server in the folder that contains the js QTI Engine
-#' that performs the conversion of qti.xml into HTML.
+#' Uses QTIJS to render a single xml file in the RStudio viewer pane with a
+#' local server.
 #'
-#' @param qti_engine_f filepath to the js QTI Engine. Can be empty to indicate
-#'   that the server can be initiated in the package folder.
+#' @param input input file
+#' @return nothing, has side effects
+#' @export
+render_xml <- function(input) {
+  renderer <- prepare_renderer()
+  # use index.xml for a single file
+  file.copy(input, paste0(renderer$path, "/index.xml"))
+  rstudioapi::viewer(renderer$url)
+}
+
+#' Start QTIJS on a local server
+#'
+#' This function starts an http server with the QTIJS renderer. The renderer performs the conversion of qti.xml into HTML.
+#'
+#' The server has to be started manually by the user, otherwise the Knit Button will not work. The Button starts a new session and invoking a server there does not make much sense.
 #'
 #' @examples
 #' \dontrun{
 #' # Initiated server in qtiViewer folder
 #' start_server()
 #' # Initiated server in a specific folder provided by the user. This folder
-#'  contains the js QTI Engine
-#' start_server("/pathToTheQtiEngine/")
+#'  contains the QTI renderer
+#' start_server("/pathToTheQtiRenderer/")
 #' }
 #' @export
-start_server <- function(qti_engine_f) {
-  servr::daemon_stop(which = servr::daemon_list())
-  if (missing(qti_engine_f)) {
-    package_d <- paste(fs::path_package("qti"), "QTI_Engine", sep = "/")
-  } else {
-    package_d <- paste(qti_engine_f)
-  }
+start_server <- function() {
+    path = qtijs_path()
+    # this will kill all servers that were started via
+    # servr in the session; maybe this is not necessary
+    servr::daemon_stop(which = servr::daemon_list())
+    server_info <- servr::httw(dir = path)
+    # only way to get the url when using Knit Button
+    Sys.setenv("QTI_URL" = server_info$url)
+    return(server_info$url)
+}
 
-  if (file.exists(package_d)) {
-    server_info <- servr::httw(dir = package_d)
-    port_s <- server_info[2]
-    Sys.setenv(QTI_PORT = port_s$port)
-    Sys.setenv(QTI_FOLDER = package_d)
-  } else {
-    print("QTI Engine not found in directory")
+
+#' shortcut for the correct QTIJS path
+qtijs_path <- function() {
+    fs::path_package("qti", "QTIJS")
+}
+#' Prepare QTIJS renderer
+#'
+#' Starts server for QTIJS, returns path of QTIJS and the url of the server.
+prepare_renderer <- function() {
+  path <- qtijs_path()
+  # start a server if none are there or there is no server url
+  if (length(servr::daemon_list()) == 0 || Sys.getenv("QTI_URL") == "") {
+      start_server()
   }
+  # clean up
+  files_d <- paste(path, "*.xml", sep = "/")
+  unlink(files_d)
+  return(list(path = path, url = Sys.getenv("QTI_URL")))
 }
