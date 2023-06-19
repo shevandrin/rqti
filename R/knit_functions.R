f642819e639a543db1e3a2aec94281cad0ec249b
Johannes Titz <johannes@titz.science>
2023-06-19 09:18:53 +0200

    refactor(renderer): improve names, checks and messages
    - knit_qti_html is now called render_rmd
    - render_rmd checks for interactive session, if not interactive (Knit button) the function is stopped, otherwise the server is started automatically
    - more informative message is shown for render_rmd
    - xml file is created when Knit button is clicked
    - rstudio viewer pane is only run when in RStudio
    - custom start up message when server is started
    - checks for server up are only based on environment variable QTI_URL
    - stop_server function added

--- R/knit_functions.R
+++ R/knit_functions.R
@@ -1,11 +1,16 @@
 #' Render an RMD file as qti xml with QTIJS
 #'
-#' Generates the qti xml file through the functions of the package exams. The
-#' xml is copied into the QTIJS folder which transforms the xml into HTML.
-#' Finally, the HTML is displayed and the user can have a preview of the
-#' exercise or exam.
+#' Generates the qti xml file via rmd2xml. The xml is copied into the QTIJS
+#' folder of the package which transforms the xml into HTML. Finally, the HTML
+#' is displayed and the user can have a preview of the exercise or exam.
 #'
 #' Requires a running QTIJS server, which can be started with start_server().
+#' When loading the package qti, a server is started automatically.
+#'
+#' The preview is automatically loaded into the RStudio viewer. Alternatively
+#' you  can just open the browser in the corresponding local server which is
+#' displayed when rendering. Since the function is supposed to be called via the
+#' Knit-Button in RStudio, it defaults to the RStudio viewer pane.
 #'
 #' @param input (the path to the input Rmd document)
 #' @param ... required for passing arguments when knitting
@@ -13,19 +18,28 @@
 #' @examples
 #' # Customize knit function in the Rmd file using the following YAML setting
 #' # after the word knit:
-#' \dontrun{qti::knit}
+#' \dontrun{qti::knit_qti}
 #'
 #' @export
-knit_qti_html <- function(input, ...) {
-    # naming this function just knit, does not work
+render_rmd <- function(input, ...) {
     clean_qtijs()
+    # for render_rmd this has to be checked manually because the Knit Button
+    # is tricky to set up
     url <- Sys.getenv("QTI_URL")
-    print(url)
     if (url == "") {
-      stop("Server for QTIJS not running. You can start it manually with start_server()")
+      # Knit Button runs in separate session
+      if (!interactive()) {
+          stop("Server for QTIJS not running. You can start it manually with start_server()")
+      } else {
+          url <- prepare_renderer()
+      }
     }
-    rmd2zip(input, path = qtijs_path())
-    rstudioapi::viewer(url)
+    message("Open browser at: ", url, " for preview")
+    rmd2xml(input, path = paste0(qtijs_path(), "/index.xml"))
+    rmd2xml(input)
+    if (Sys.getenv("RSTUDIO") == "1") {
+        rstudioapi::viewer(url)
+    }
 }
 
 #' Render a single xml file
@@ -40,7 +54,9 @@ render_xml <- function(input) {
   url <- prepare_renderer()
   # use index.xml for a single file
   file.copy(input, paste0(qtijs_path(), "/index.xml"))
-  rstudioapi::viewer(url)
+  if (Sys.getenv("RSTUDIO") == "1") {
+        rstudioapi::viewer(url)
+  }
 }
 
 #' Render a zipped qti archive
@@ -53,7 +69,9 @@ render_xml <- function(input) {
 render_zip <- function(input) {
     url <- prepare_renderer()
     unzip(input, exdir = qtijs_path())
-    rstudioapi::viewer(url)
+    if (Sys.getenv("RSTUDIO") == "1") {
+        rstudioapi::viewer(url)
+    }
 }
 
 #' Start QTIJS on a local server
@@ -76,7 +94,8 @@ start_server <- function() {
     # this will kill all servers that were started via
     # servr in the session; maybe this is not necessary
     servr::daemon_stop(which = servr::daemon_list())
-    server_info <- servr::httw(dir = path)
+    server_info <- servr::httw(dir = path, verbose = F)
+    message("To stop the server, run stop_server(). If you restart the R session, the server is restarted, too. Call start_server() to manually (re)start the server.\nServing the directory ", path, " at ", server_info$url)
     # only way to get the url when using Knit Button
     Sys.setenv("QTI_URL" = server_info$url)
     return(server_info$url)
@@ -93,7 +112,7 @@ qtijs_path <- function() {
 prepare_renderer <- function() {
   path <- qtijs_path()
   # start a server if none are there or there is no server url
-  if (length(servr::daemon_list()) == 0 || Sys.getenv("QTI_URL") == "") {
+  if (Sys.getenv("QTI_URL") == "") {
       start_server()
   }
   # clean up
@@ -103,4 +122,11 @@ prepare_renderer <- function() {
 
 clean_qtijs <- function() {
   unlink(paste(qtijs_path(), "*.xml", sep = "/"))
+}
+
+#' Stop QTIJS local server
+#' @export
+stop_server <- function() {
+    Sys.setenv(QTI_URL = "")
+    servr::daemon_stop()
 }
