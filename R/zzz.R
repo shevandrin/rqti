73493d746b20013ffe31e94ebeb1ab1436080c60
Johannes Titz <johannes@titz.science>
2023-06-19 10:16:03 +0200

    fix(renderer): correct loading function, add .onDetach

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,10 @@
-.onLoad <- function(libname, pkgname){
+.onAttach <- function(libname, pkgname){
     if (interactive()) {
         packageStartupMessage("Welcome to qti.\nStarting QTIJS rendering server.")
         start_server()
     }
+}
+
+.onDetach <- function(libpath) {
+    stop_server()
 }
