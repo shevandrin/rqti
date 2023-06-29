b4ebcb19ba3d7e3d294d68846b9ffffb4da34b5f
Andrey Shevandrin <shevandrin@gmail.com>
2023-06-29 09:38:15 +0200

    fix: fix error with outcome of gsub in parse_list() in object_builder()

--- R/objectBuilder.R
+++ R/objectBuilder.R
@@ -265,8 +265,7 @@ parse_list <- function(html) {
     choices  <- xml2::xml_find_all(question_list, "//li")
     em <- xml2::xml_text(xml2::xml_find_all(question_list, ".//em"))
     solution <- which(xml2::xml_text(choices) %in% em)
-    choices <- gsub("<li>|</li>", "", as.character(choices))
-    choices <- choices[nzchar(choices)]
+    choices <- gsub("<li>(.*?)</li>", "\\1", as.character(choices))
     xml_remove(question_list[length(question_list)])
     return(list(choices = choices, solution = solution))
 }
