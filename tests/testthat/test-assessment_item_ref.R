617ac0c18a17d2e9115c36ff414d811bce7ff474
Peter <bondarenko.volsu@gmail.com>
2023-07-18 14:50:54 +0200

    style: test-assessment_item_ref.R

--- tests/testthat/test-assessment_item_ref.R
+++ tests/testthat/test-assessment_item_ref.R
@@ -1,20 +1,20 @@
-test_that("Testing method buildAssessmentSection() for AssessmentItemRef class", {
+test_that("Testing method buildAssessmentSection() for AssessmentItemRef class",
+          {
     sut <- AssessmentItemRef (identifier = "ID_1",
                               href = "test_create_qti_task_SingleChoice.xml"
     )
     example <-"
     <assessmentItemRef identifier=\"ID_1\" href=\"test_create_qti_task_SingleChoice.xml\"></assessmentItemRef>"
-    xml1 <- xml2::read_xml(toString(buildAssessmentSection(sut)))
-    xml2 <- xml2::read_xml(example)
-    expect_equal(xml1, xml2)
+    sut <- xml2::read_xml(toString(buildAssessmentSection(sut)))
+    expected <- xml2::read_xml(example)
+    expect_equal(sut, expected)
 })
+
 test_that("Testing method getAssessmentItems() for AssessmentItemRef class", {
     sut <- AssessmentItemRef (identifier = "ID_1",
-                              href = "test_create_qti_task_SingleChoice.xml"
-    )
-    a <- toString(getAssessmentItems(sut))
+                              href = "test_create_qti_task_SingleChoice.xml")
 
-    example <- "test_create_qti_task_SingleChoice.xml"
-    extended <- toString(getAssessmentItems(sut))
-    expect_equal(example, extended)
+    expected <- "test_create_qti_task_SingleChoice.xml"
+    sut <- toString(getAssessmentItems(sut))
+    expect_equal(sut, expected)
 })
