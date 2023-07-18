f42c0c1c74b7af37e14eb66e57097487d8a77d2c
Peter <bondarenko.volsu@gmail.com>
2023-07-18 14:10:34 +0200

    style: test-multiple_choice_table.R

--- tests/testthat/test-multiple_choice_table.R
+++ tests/testthat/test-multiple_choice_table.R
@@ -1,9 +1,11 @@
-# ' QTI example does not provide Outcome declaration then the following example was taken from OPAL
+# QTI example does not provide Outcome declaration then the following example
+# was taken from OPAL
 test_that("Testing CreateItemBody MultipleChoiceTable", {
     sc <- new("MultipleChoiceTable",
               rows = c("Capulet", "Demetrius", "Lysander", "Prospero"),
               rows_identifiers  = c("C", "D", "L", "P"),
-              cols = c("A Midsummer-Night's Dream", "Romeo and Juliet", "The Tempest"),
+              cols = c("A Midsummer-Night's Dream", "Romeo and Juliet",
+                       "The Tempest"),
               cols_identifiers = c("M", "R", "T"),
               answers_identifiers = c("C R", "C T", "D M", "L M", "P T", "P R"),
               answers_scores = c(1, 0.5, 0.5, 0.5, 1, 1),
@@ -13,6 +15,7 @@ test_that("Testing CreateItemBody MultipleChoiceTable", {
 
 # this example has been changed by Andrey. Values of matchMax has to be 2 or
 # more in multipleChoiceTable
+
     example <- '<itemBody>
 <matchInteraction responseIdentifier="RESPONSE" shuffle="true" maxAssociations="4">
 <prompt>Match the following characters to the Shakespeare play they appeared in:</prompt>
@@ -30,9 +33,9 @@ test_that("Testing CreateItemBody MultipleChoiceTable", {
         </matchInteraction>
         </itemBody>'
 
-    xml1 <- xml2::read_xml(toString(createItemBody(sc)))
-    xml2 <- xml2::read_xml(example)
-    expect_equal(xml1, xml2)
+    sut <- xml2::read_xml(toString(createItemBody(sc)))
+    expected <- xml2::read_xml(example)
+    expect_equal(sut, expected)
 })
 
 test_that("Testing create_response_declaration_MultipleChoiceTable",{
@@ -74,9 +77,9 @@ test_that("Testing create_response_declaration_MultipleChoiceTable",{
 </responseDeclaration>'
 
     qtiXML <- toString(createResponseDeclaration(sc))
-    xml1 <- xml2::read_xml(qtiXML)
-    xml2 <- xml2::read_xml(example)
-    expect_equal(xml1, xml2)
+    sut <- xml2::read_xml(qtiXML)
+    expected <- xml2::read_xml(example)
+    expect_equal(sut, expected)
 
 })
 
@@ -84,7 +87,8 @@ test_that("Testing outcomeDeclaration MultipleChoiceTable",{
     sc <- new("MultipleChoiceTable",
               rows = c("Capulet", "Demetrius", "Lysander", "Prospero"),
               rows_identifiers  = c("C", "D", "L", "P"),
-              cols = c("A Midsummer-Night's Dream", "Romeo and Juliet", "The Tempest"),
+              cols = c("A Midsummer-Night's Dream", "Romeo and Juliet",
+                       "The Tempest"),
               cols_identifiers = c("M", "R", "T"),
               answers_identifiers = c("C R", "C T", "D M", "L M", "P T", "P R"),
               answers_scores = c(1, 0.5, 0.5, 0.5, 1, 1),
@@ -93,7 +97,8 @@ test_that("Testing outcomeDeclaration MultipleChoiceTable",{
               prompt = "Match the following characters to the Shakespeare play they appeared in:"
     )
 
-    # ' QTI example does not provide Outcome declaration then the following example was taken from OPAL
+    # QTI example does not provide Outcome declaration then the following
+    # example was taken from OPAL
     example <- '<additionalTag><outcomeDeclaration identifier="SCORE" cardinality="single" baseType="float">
 <defaultValue>
 <value>0</value>
@@ -111,9 +116,10 @@ test_that("Testing outcomeDeclaration MultipleChoiceTable",{
 </outcomeDeclaration>
     </additionalTag>'
 
-    responseDe <- as.character(htmltools::tag("additionalTag", list(createOutcomeDeclaration(sc))))
-    xml1 <- xml2::read_xml(responseDe)
-    xml2 <- xml2::read_xml(example)
-    expect_equal(xml1, xml2)
+    responseDe <- as.character(htmltools::tag(
+        "additionalTag", list(createOutcomeDeclaration(sc))))
+    sut <- xml2::read_xml(responseDe)
+    example <- xml2::read_xml(example)
+    expect_equal(sut, example)
 
 })
