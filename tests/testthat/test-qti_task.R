480d4fd7f82d8039e399c3e0253165a323e8cb2b
Peter <bondarenko.volsu@gmail.com>
2023-07-18 14:11:53 +0200

    style: test-qti_task.R

--- tests/testthat/test-qti_task.R
+++ tests/testthat/test-qti_task.R
@@ -1,21 +1,22 @@
 # Essay
 test_that("create_qti_task", {
     path <- test_path("file/test_create_qti_task_Essay.xml")
-    sut <- readLines(path)
+    expected <- readLines(path)
 
     essay <- new("Essay", prompt = "Test task",
                  title = "Essay",
                  identifier = "new")
     suppressMessages(create_qti_task(essay))
-    expected <- readLines("new.xml")
+    sut <- readLines("new.xml")
 
     expect_equal(sut, expected)
     file.remove("new.xml")
 })
+
 # MultipleChoice
 test_that("create_qti_task", {
     path <- test_path("file/test_create_qti_task_MultipleChoice.xml")
-    cqt <- readLines(path)
+    expected <- readLines(path)
     mc <- new("MultipleChoice",
                identifier = "new", prompt = "What does 3/4 + 1/4 = ?",
                title = "MultipleChoice",
@@ -24,15 +25,16 @@ test_that("create_qti_task", {
                points = c(1, 0, 0, 1)
     )
     suppressMessages(create_qti_task(mc))
-    expected <- readLines("new.xml")
+    sut <- readLines("new.xml")
 
-    expect_equal(cqt, expected)
+    expect_equal(sut, expected)
     file.remove("new.xml")
 })
+
 # SingleChoice
 test_that("create_qti_task", {
     path <- test_path("file/test_create_qti_task_SingleChoice.xml")
-    cqt <- readLines(path)
+    expected <- readLines(path)
     sc <- new("SingleChoice",
                prompt = "What is the percentage of 3/20?",
                title = "SingleChoice",
@@ -40,15 +42,16 @@ test_that("create_qti_task", {
                choice_identifiers = "1",
                identifier = "new")
     suppressMessages(create_qti_task(sc))
-    expected <- readLines("new.xml")
+    sut <- readLines("new.xml")
 
-    expect_equal(cqt, expected)
+    expect_equal(sut, expected)
     file.remove("new.xml")
 })
+
 # Order
 test_that("create_qti_task", {
     path <- test_path("file/test_create_qti_task_Order.xml")
-    cqt <- readLines(path)
+    expected <- readLines(path)
     order <- new("Order",
                   identifier = "new",
                   title = "Order",
@@ -60,82 +63,88 @@ test_that("create_qti_task", {
                   points = 1,
                  points_per_answer = FALSE)
     suppressMessages(create_qti_task(order))
-    expected <- readLines("new.xml")
+    sut <- readLines("new.xml")
 
-    expect_equal(cqt, expected)
+    expect_equal(sut, expected)
     file.remove("new.xml")
 })
+
 # OneInColTable
 test_that("create_qti_task", {
     path <- test_path("file/test_create_qti_task_OneInColTable.xml")
-    cqt <- readLines(path)
+    expected <- readLines(path)
     oneInColTable <- new("OneInColTable",
                          content = list("<p>\"One in col\" table task</p>",
                                         "<i>table description</i>"),
-                         identifier = "new",
-                         title = "OneInColTable",
-                         prompt = "Choose the correct order in the multiplication table",
-                         rows = c("4*7 =", "3*9 =", "5*5 =", "2*3 =", "12*3 ="),
-                         rows_identifiers = c("a", "b", "c", "d", "e"),
-                         cols = c("27", "36", "25", "6", "72/2"),
-                         cols_identifiers = c("k", "l", "m", "n", "p"),
-                         answers_identifiers =c("b k", "c m", "d n", "e l", "e p"),
-                         points = 5
+                 identifier = "new",
+                 title = "OneInColTable",
+                 prompt = "Choose the correct order in the multiplication table",
+                 rows = c("4*7 =", "3*9 =", "5*5 =", "2*3 =", "12*3 ="),
+                 rows_identifiers = c("a", "b", "c", "d", "e"),
+                 cols = c("27", "36", "25", "6", "72/2"),
+                 cols_identifiers = c("k", "l", "m", "n", "p"),
+                 answers_identifiers =c("b k", "c m", "d n", "e l", "e p"),
+                 points = 5
     )
     suppressMessages(create_qti_task(oneInColTable))
-    expected <- readLines("new.xml")
+    sut <- readLines("new.xml")
 
-    expect_equal(cqt, expected)
+    expect_equal(sut, expected)
     file.remove("new.xml")
 })
+
 # OneInRowTable
 test_that("create_qti_task", {
     path <- test_path("file/test_create_qti_task_OneInRowTable.xml")
-    cqt <- readLines(path)
-    OneInRowTable <- new("OneInRowTable", content = list("<p>\"One in row\" table task</p>",
-                                                         "<i>table description</i>"),
-                         identifier = "new",
-                         title = "OneInRowTable",
-                         prompt = "Choose the correct order in the multiplication table",
-                         rows = c("4*9 =", "3*9 =", "5*5 =", "2*3 =", "12*3 ="),
-                         rows_identifiers = c("a", "b", "c", "d", "e"),
-                         cols = c("27", "36", "25", "6"),
-                         cols_identifiers = c("k", "l", "m", "n"),
-                         answers_identifiers = c("a l", "b k", "c m", "d n", "e l"),
-                         points = 5)
+    expected <- readLines(path)
+    OneInRowTable <- new("OneInRowTable",
+                         content = list("<p>\"One in row\" table task</p>",
+                                                   "<i>table description</i>"),
+             identifier = "new",
+             title = "OneInRowTable",
+             prompt = "Choose the correct order in the multiplication table",
+             rows = c("4*9 =", "3*9 =", "5*5 =", "2*3 =", "12*3 ="),
+             rows_identifiers = c("a", "b", "c", "d", "e"),
+             cols = c("27", "36", "25", "6"),
+             cols_identifiers = c("k", "l", "m", "n"),
+             answers_identifiers = c("a l", "b k", "c m", "d n", "e l"),
+             points = 5)
     suppressMessages(create_qti_task(OneInRowTable))
-    expected <- readLines("new.xml")
+    sut <- readLines("new.xml")
 
-    expect_equal(cqt, expected)
+    expect_equal(sut, expected)
     file.remove("new.xml")
 })
+
 # MultipleChoiceTable
 test_that("create_qti_task", {
     path <- test_path("file/test_create_qti_task_MultipleChoiceTable.xml")
-    cqt <- readLines(path)
+    expected <- readLines(path)
+
     MultipleChoiceTable <- new("MultipleChoiceTable",
-                               content = list("<p>\"One in col\" table task</p>",
-                                              "<i>table description</i>"),
-                               identifier = "new",
-                               title = "MultipleChoiceTable",
-                               prompt = "Choose the correct order in the multiplication table",
-                               rows = c("4*7 =", "3*9 =", "5*5 =", "2*3 =", "12*3 ="),
-                               rows_identifiers = c("a", "b", "c", "d", "e"),
-                               cols = c("27", "36", "25", "6", "72/2"),
-                               cols_identifiers = c("k", "l", "m", "n", "p"),
-                               answers_identifiers =c("b k", "c m", "d n", "e l", "e p"),
-                               points = 5
+                content = list("<p>\"One in col\" table task</p>",
+                              "<i>table description</i>"),
+                identifier = "new",
+                title = "MultipleChoiceTable",
+                prompt = "Choose the correct order in the multiplication table",
+                rows = c("4*7 =", "3*9 =", "5*5 =", "2*3 =", "12*3 ="),
+                rows_identifiers = c("a", "b", "c", "d", "e"),
+                cols = c("27", "36", "25", "6", "72/2"),
+                cols_identifiers = c("k", "l", "m", "n", "p"),
+                answers_identifiers =c("b k", "c m", "d n", "e l", "e p"),
+                points = 5
     )
     suppressMessages(create_qti_task(MultipleChoiceTable))
-    expected <- readLines("new.xml")
+    sut <- readLines("new.xml")
 
-    expect_equal(cqt, expected)
+    expect_equal(sut, expected)
     file.remove("new.xml")
 })
+
 # DirectedPair
 test_that("create_qti_task", {
     path <- test_path("file/test_create_qti_task_DirectedPair.xml")
-    cqt <- readLines(path)
+    expected <- readLines(path)
     DirectedPair <- new("DirectedPair",
                     content = list("<p>\"Directed pairs\" task</p>"),
                     identifier = "new",
@@ -148,11 +157,12 @@ test_that("create_qti_task", {
                     points = 5
 )
     suppressMessages(create_qti_task(DirectedPair))
-    expected <- readLines("new.xml")
+    sut <- readLines("new.xml")
 
-    expect_equal(cqt, expected)
+    expect_equal(sut, expected)
     file.remove("new.xml")
 })
+
 # TextGapOpal
 test_that("create_qti_task", {
     path <- test_path("file/test_create_qti_task_TextGapOpal.xml")
@@ -175,6 +185,7 @@ TextGapOpal <- new("Entry",
     expect_equal(sut, expected)
     file.remove("new.xml")
 })
+
 # NumericGap
 test_that("create_qti_task", {
     path <- test_path("file/test_create_qti_task_NumericGap.xml")
@@ -197,10 +208,11 @@ NumericGap <- new("Entry",
     expect_equal(sut, expected)
     file.remove("new.xml")
 })
+
 # InlineChoice
 test_that("create_qti_task", {
     path <- test_path("file/test_create_qti_task_InlineChoice.xml")
-    cqt <- readLines(path)
+    expected <- readLines(path)
 InlineChoice <- new("Entry",
                     identifier = "new",
                     points = 1,
@@ -214,8 +226,8 @@ InlineChoice <- new("Entry",
                                    'm/s')
 )
     suppressMessages(create_qti_task(InlineChoice))
-    expected <- readLines("new.xml")
+    sut <- readLines("new.xml")
 
-    expect_equal(cqt, expected)
+    expect_equal(sut, expected)
     file.remove("new.xml")
 })
