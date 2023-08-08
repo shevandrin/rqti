8203357c0cc0173ca1fa4a269cf6d1a333b1c0a5
Peter <bondarenko.volsu@gmail.com>
2023-08-08 12:15:46 +0200

    refactor: test-multiple_choice.R

--- tests/testthat/test-multiple_choice.R
+++ tests/testthat/test-multiple_choice.R
@@ -1,12 +1,14 @@
-test_that("Testing create_item_body_multiplechoice", {
-    sc <- new("MultipleChoice",
+test_that("Testing  method of createItemBody
+          for MultipleChoice class", {
+    mc <- new("MultipleChoice",
               content = list(),
               choices = c("Hydrogen","Helium","Carbon","Oxygen","Nitrogen",
                           "Chlorine"),
               choice_identifiers = c("H","He","C","O","N","Cl"),
               points = c(1,0,0,1,0,-1),
               title = "filename_sc",
-              prompt = "Which of the following elements are used to form water?")
+              prompt = paste0("Which of the following elements ",
+                              "are used to form water?"))
 
     example <- '<itemBody>
 <choiceInteraction responseIdentifier="RESPONSE" shuffle="true" maxChoices="0"
@@ -21,24 +23,29 @@ orientation="vertical">
 </choiceInteraction>
 </itemBody>'
 
-    sut <- xml2::read_xml(toString(create_item_body_multiplechoice(sc)))
+    sut <- xml2::read_xml(toString(createItemBody(mc)))
     expected <- xml2::read_xml(example)
     expect_equal(sut, expected)
 })
 
-test_that("Testing create_response_declaration_multiple_choice",{
+test_that("Testing method of createResponseDeclaration
+          for MultipleChoice class",{
     skip_if_not_installed("XML")
-    sc <- new("MultipleChoice",
+    mc <- new("MultipleChoice",
               content = list(""),
               choices = c("Hydrogen","Helium","Carbon","Oxygen","Nitrogen",
                           "Chlorine"),
               choice_identifiers = c("H","He","C","O","N","Cl"),
               points = c(1,0,0,1,0,-1),
               title = "filename_sc",
-              prompt = "Which of the following elements are used to form water?")
-# 'The default value was changed from -2 to 0 because the package not allow negative points for Total score
+              prompt = paste0("Which of the following elements ",
+                                       "are used to form water?"))
 
-    example <- '<responseDeclaration identifier="RESPONSE" cardinality="multiple" baseType="identifier">
+    # The default value was changed from -2 to 0 because the package
+    # not allow negative points for Total score
+
+    example <- '<responseDeclaration identifier="RESPONSE"
+                            cardinality="multiple" baseType="identifier">
     <correctResponse>
         <value>H</value>
         <value>O</value>
@@ -50,27 +57,32 @@ test_that("Testing create_response_declaration_multiple_choice",{
     </mapping>
     </responseDeclaration>'
 
-    qtiXML <- toString(create_response_declaration_multiple_choice(sc))
+    qtiXML <- toString(createResponseDeclaration(mc))
     sut <- xml2::read_xml(qtiXML)
     expected <- xml2::read_xml(example)
 
     if(isTRUE(identical(sut,expected)) == FALSE){
 
-        # 'Convert the String in atomic vector to compare the 2 XML are equal in content
+    # Convert the String in atomic vector to compare
+    # the 2 XML are equal in content
         vxml1 <- unlist(strsplit(as.character(sut),split = ""))
         vxml2 <- unlist(strsplit(as.character(expected),split = ""))
 
-        if (identical(stringr::str_sort(vxml2), stringr::str_sort(vxml1)) == FALSE){
+        if (identical(stringr::str_sort(vxml2),
+                      stringr::str_sort(vxml1)) == FALSE){
             print("XML content differs")
             expect_equal(sut, expected)
         }
         else{
-            #compare if the 2 XML are equal in structure. It omits attributes and other values, only validate de tag names
+            # Compare if the 2 XML are equal in structure.
+            # It omits attributes and other values, only validate de tag names
             a <- XML::xmlParse(sut, asText = TRUE)
             b <- XML::xmlParse(expected, asText = TRUE)
             result <- XML::compareXMLDocs(a,b)
 
-            if(is.logical(result$countDiffs) && length(result$countDiffs) == 1 && !is.na(result$countDiffs) && lengths(result$countDiffs) > 0){
+            if(is.logical(result$countDiffs) &&
+               length(result$countDiffs) == 1 &&
+               !is.na(result$countDiffs) && lengths(result$countDiffs) > 0){
                 print("XML structure differs")
                 expect_equal(sut, expected)
             }
@@ -85,122 +97,49 @@ test_that("Testing create_response_declaration_multiple_choice",{
 
 })
 
-test_that("Testing outcomeDeclaration for Multiple Choice",{
-    sc <- new("MultipleChoice",
-              content = list(""),
-              choices = c("Hydrogen","Helium","Carbon","Oxygen","Nitrogen",
-                          "Chlorine"),
-              points = c(1,0,0,1,0,-1),
-              title = "filename_sc",
-              prompt = "Which of the following elements are used to form water?")
+test_that("Testing  method of getPoints for MultipleChoice class", {
+              mc <- new("MultipleChoice",
+                        content = list(),
+                        choices = c("Hydrogen","Helium","Carbon",
+                                    "Oxygen","Nitrogen",
+                                    "Chlorine"),
+                        choice_identifiers = c("H","He","C","O","N","Cl"),
+                        points = c(1,0,0,1,0,-1),
+                        title = "filename_sc",
+                        prompt = paste0("Which of the following elements ",
+                                        "are used to form water?"))
 
-    #' The original example does not include this part, the content of the example is based on OPAL configuration
-    example <- '<outcomeDeclaration identifier="MAXSCORE" cardinality="single" baseType="float">
-<defaultValue>
-<value>2</value>
-</defaultValue>
-</outcomeDeclaration>'
+              sut <- getPoints(mc)
 
-    sut <- xml2::read_xml(
-        toString(create_outcome_declaration_multiple_choice(sc)))
-    expected <- xml2::read_xml(example)
-    expect_equal(sut, expected)
-})
+             # To calculate only positive points points for
+             # MultipleChoice class
 
-# Testing with modal Feedback
-test_that("Testing outcomeDeclaration for Multiple Choice",{
-    mc <- new("MultipleChoice",
-              content = list(""),
-              choices = c("Hydrogen","Helium","Carbon","Oxygen","Nitrogen",
-                          "Chlorine"),
-              points = c(1,0,0,1,0,-1),
-              title = "filename_sc",
-              prompt = "Which of the following elements are used to form water?",
-              feedback = list(new("ModalFeedback", title = "common",
-                                  content = list("general feedback"))))
+              positive_points <- mc@points[mc@points > 0]
+              expected <- sum(positive_points)
 
-    example <- '
-    <responseProcessing>
-  <responseCondition>
-    <responseIf>
-      <not>
-        <isNull>
-          <variable identifier="RESPONSE"></variable>
-        </isNull>
-      </not>
-      <setOutcomeValue identifier="SCORE">
-        <sum>
-          <variable identifier="SCORE"></variable>
-          <mapResponse identifier="RESPONSE"></mapResponse>
-        </sum>
-      </setOutcomeValue>
-    </responseIf>
-  </responseCondition>
-  <responseCondition>
-    <responseIf>
-      <gt>
-        <variable identifier="SCORE"></variable>
-        <variable identifier="MAXSCORE"></variable>
-      </gt>
-      <setOutcomeValue identifier="SCORE">
-        <variable identifier="MAXSCORE"></variable>
-      </setOutcomeValue>
-    </responseIf>
-  </responseCondition>
-  <responseCondition>
-    <responseIf>
-      <lt>
-        <variable identifier="SCORE"></variable>
-        <variable identifier="MINSCORE"></variable>
-      </lt>
-      <setOutcomeValue identifier="SCORE">
-        <variable identifier="MINSCORE"></variable>
-      </setOutcomeValue>
-    </responseIf>
-  </responseCondition>
-  <responseCondition>
-    <responseIf>
-      <isNull>
-        <variable identifier="RESPONSE"></variable>
-      </isNull>
-      <setOutcomeValue identifier="FEEDBACKBASIC">
-        <baseValue baseType="identifier">empty</baseValue>
-      </setOutcomeValue>
-    </responseIf>
-    <responseElseIf>
-      <lt>
-        <variable identifier="SCORE"></variable>
-        <variable identifier="MAXSCORE"></variable>
-      </lt>
-      <setOutcomeValue identifier="FEEDBACKBASIC">
-        <baseValue baseType="identifier">incorrect</baseValue>
-      </setOutcomeValue>
-    </responseElseIf>
-    <responseElse>
-      <setOutcomeValue identifier="FEEDBACKBASIC">
-        <baseValue baseType="identifier">correct</baseValue>
-      </setOutcomeValue>
-    </responseElse>
-  </responseCondition>
-  <responseCondition>
-    <responseIf>
-      <and>
-        <gte>
-          <variable identifier="SCORE"></variable>
-          <baseValue baseType="float">0</baseValue>
-        </gte>
-      </and>
-      <setOutcomeValue identifier="FEEDBACKMODAL">
-        <multiple>
-          <variable identifier="FEEDBACKMODAL"></variable>
-          <baseValue baseType="identifier">modal_feedback</baseValue>
-        </multiple>
-      </setOutcomeValue>
-    </responseIf>
-  </responseCondition>
-</responseProcessing>
-    '
-    sut <- xml2::read_xml(toString(createResponseProcessing(mc)))
-    expected <- xml2::read_xml(example)
-    expect_equal(sut, expected)
-})
+              expect_equal(sut, expected)
+          })
+
+test_that("Testing outcomeDeclaration() for Multiple Choice",{
+        mc <- new("MultipleChoice",
+                  content = list(""),
+                  choices = c("Hydrogen","Helium","Carbon","Oxygen","Nitrogen",
+                              "Chlorine"),
+                  points = c(1,0,0,1,0,-1),
+                  title = "filename_sc",
+                  prompt = paste0("Which of the following elements ",
+                                           "are used to form water?"))
+
+        example <- '
+    <outcomeDeclaration identifier="MAXSCORE"
+                            cardinality="single" baseType="float">
+    <defaultValue>
+    <value>2</value>
+    </defaultValue>
+    </outcomeDeclaration>'
+
+        sut <- xml2::read_xml(
+            toString(create_outcome_declaration_multiple_choice(mc)))
+        expected <- xml2::read_xml(example)
+        expect_equal(sut, expected)
+    })
