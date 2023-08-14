828cf794e0a8385c7283989ce3d1437bfa862406
Peter <bondarenko.volsu@gmail.com>
2023-08-14 09:51:27 +0200

    test: add helper function equal_xml()

---
+++ tests/testthat/test_helpers.R
@@ -0,0 +1,21 @@
+# Define the test-only function
+equal_xml <- function(sut, expected) {
+  parse_sut <- XML::xmlParse(sut, asText = TRUE)
+  parse_expected <- XML::xmlParse(expected, asText = TRUE)
+
+  result <- XML::compareXMLDocs(parse_sut, parse_expected)
+
+  v_xml_sut <- unlist(strsplit(as.character(sut), split = ""))
+  v_xml_expected <- unlist(strsplit(as.character(expected), split = ""))
+
+  sort_v_xml_sut <- sort(v_xml_sut)
+  sort_v_xml_expected <- sort(v_xml_expected)
+
+  if (identical(sort_v_xml_sut, sort_v_xml_expected) &&
+        length(result$countDiffs) == 0) {
+    return(expect_equal(0, 0))
+  } else {
+    print("XML structures are differs")
+    return(expect_equal(sut, expected))
+  }
+}
