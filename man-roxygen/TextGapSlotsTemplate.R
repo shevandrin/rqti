e8aa72043b45afa8616a5f2e34e737ff12c8934b
Andrey Shevandrin <shevandrin@gmail.com>
2023-10-18 14:23:54 +0200

    docs: change slot description for TextGap issue #87

--- man-roxygen/TextGapSlotsTemplate.R
+++ man-roxygen/TextGapSlotsTemplate.R
@@ -1,4 +1,4 @@
 #' @slot solution string; contains a vector of values that are
 #'   considered as correct answers
 #' @slot case_sensitive logical; determines whether the evaluation of the
-#'   correct answer is case sensitive; default `TRUE`
+#'   correct answer is case sensitive; default `FALSE`
