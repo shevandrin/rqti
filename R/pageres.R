c964ee26f13e0cdbe868f4a2e20711c4c5e76573
Johannes Titz <johannes@titz.science>
2023-10-31 10:55:11 +0100

    feat: pagesres for opal repo
    - needs imagemagick fro trimming

--- R/pageres.R
+++ R/pageres.R
@@ -4,3 +4,13 @@ pageres <- function(filename, res) {
                   " --overwrite --filename=", filename, " ", res),
            wait = F)
 }
+
+pageres_opal <- function(repo, path) {
+    file <- paste0(path, repo$display_name, ".jpg")
+    webshot2::webshot(url = repo$url,
+                      file = file,
+                      selector = ".content-container-inner",
+                      expand = c(3, 9, 0, -6), vheight = 1200, vwidth = 1100)
+    # requires imagemagick
+    system(paste0("convert ", file, " -trim -border 3 ", file))
+}
