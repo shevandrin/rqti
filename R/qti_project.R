69f430fb6b6c3f60f36dae6189cb4e2c3da2ce8b
Andrey Shevandrin <shevandrin@gmail.com>
2024-01-08 11:15:45 +0100

    feat: add starting R script main.R in QTI Project template issue #122

--- R/qti_project.R
+++ R/qti_project.R
@@ -11,7 +11,8 @@ qti_project <- function(path, ...) {
                "dropdown", "order", "table",
                "directedpair", "essay")
     temps_dots <- dots[temps]
-    Map(copy_template, temps_dots, names(temps_dots), path, dots['render'])
+    temps_files <- Map(copy_template, temps_dots, names(temps_dots),
+                       path, dots['render'])
 
     # create Rprofile
     text <- c(paste0("Sys.setenv(QTI_API_ENDPOINT=\"", dots$url_endpoint, "\")"),
@@ -24,6 +25,46 @@ qti_project <- function(path, ...) {
     # write to Rprofile
     writeLines(contents, con = file.path(path, ".Rprofile"))
 
+    # create default main R file
+    header <- c(
+        "# The qti package provides a powerful toolset for creating exercises",
+        "# and exams according to the QTI standard directly from R.",
+        "# This script serves as a basic introduction to the qti package,",
+        "# demonstrating workflow.",
+        "#",
+        "# Step 1. Prepare set of Rmd files with individual exercises.",
+        "# To create Rmd choose one of the Rstudio file templates starting with QTI: ….",
+        "# or edit the templates that have been copied to your working directory.\n"
+    )
+    filter_files <- Filter(Negate(is.null), temps_files)
+    print_files <- sapply(filter_files, function(x) paste0('"', x, '"'))
+    list_files <- "\"muster_exercise1.Rmd\", \"muster_exercise2.Rmd\""
+    if (length(print_files) != 0) {
+        list_files <- paste(print_files, collapse = ", ")
+    }
+    text_rmd <- c(
+        paste0("exercises = c(", list_files, ")")
+    )
+
+    text_other <- c(
+        "",
+        "# Step 2. Create sections.\n",
+        "section <- section(exercises)\n",
+        "# Step 3. Create test.\n",
+        "test <- test(section, \"muster_test\")\n",
+        "# Step 4. Upload to LMS.\n",
+        "upload2opal(test)\n"
+    )
+
+    contents <- paste(
+        paste(header, collapse = "\n"),
+        paste(text_rmd, collapse = "\n"),
+        paste(text_other, collapse = "\n"),
+        sep = "\n"
+    )
+
+    # write to default main R file
+    writeLines(contents, con = file.path(path, "main.R"))
 }
 
 copy_template <- function(need_copy, name, path, render) {
@@ -35,6 +76,7 @@ copy_template <- function(need_copy, name, path, render) {
         file_t <- file.path(path, paste0(name, ".Rmd"))
         file.copy(temp_path, file_t)
         if (render == "OPAL") replace_knit_method(file_t)
+        return(basename(file_t))
     }
 }
 
