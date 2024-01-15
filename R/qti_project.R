430d5c67886ae814c104a9ae8df16a194c496e1b
Andrey Shevandrin <shevandrin@gmail.com>
2024-01-15 16:02:57 +0100

    refactor: improvments of QTI Project templates: change variable names, add external pdf file as supplement issue #122

--- R/qti_project.R
+++ R/qti_project.R
@@ -6,6 +6,10 @@ qti_project <- function(path, ...) {
     # collect inputs
     dots <- list(...)
     sys_path <- system.file(package="qti")
+    # copy supplement file
+    file_url <- "https://raw.githubusercontent.com/johannes-titz/formelsammlung/main/main.pdf"
+    download.file(file_url, file.path(path, "demo_file.pdf") , mode = "wb",
+                  quiet = TRUE)
     # copy templates
     temps <- c("singlechoice", "multiplechoice", "dropdown", "order", "table",
                "directedpair", "essay")
@@ -13,12 +17,12 @@ qti_project <- function(path, ...) {
     if (dots$templates == "YES")  {
         text_rmd <- c(
             paste0("exercises = ", print_vector(temps, path, dots['render'])),
-            paste0("exercises_r = ", print_vector(temps_r, path, dots['render']))
+            paste0("exercises_random = ", print_vector(temps_r, path, dots['render']))
         )
     } else {
         text_rmd <- c(
            "exercises = c(\"muster_ex1.Rmd\", \"muster_ex2.Rmd\")",
-           "exercises_r = \"muster_ex_r.Rmd\""
+           "exercises_random = \"muster_ex_r.Rmd\""
         )
 
     }
@@ -49,18 +53,18 @@ qti_project <- function(path, ...) {
     text_other <- c(
         "",
         "# Step 2. Create sections.\n",
-        "permanent_section <- section(exercises)",
-        "variable_section <- section(exercises_r, n_variants = 3)",
-        "sections <- list(variable_section, permanent_section)\n",
+        "section_fixed <- section(exercises)",
+        "section_random <- section(exercises_random, n_variants = 4)",
+        "sections <- list(section_random, section_fixed)\n",
         "# Step 3. Create test.",
-        "test <- test(sections, \"muster_test_opal\", time_limits = 90,",
+        "test <- test(sections, \"test_demo\", time_limit = 90,",
         "             max_attempts = 2)\n",
         "# Create test for LMS OPAL",
-        "test_opal <- test4opal(sections, \"muster_test_opal\", time_limits = 90,",
-        "                       max_attempts = 2, files = \".Rprofile\",",
+        "test_opal <- test4opal(sections, \"test_demo_opal\", time_limit = 90,",
+        "                       max_attempts = 1, files = \"demo_file.pdf\",",
         "                       calculator = \"scientific-calculator\")",
         "# Step 4. Render Test using QTIJS server\n",
-        "zip_file <- createQtiTest(test, \"test_folder\")",
+        "zip_file <- createQtiTest(test, \"upload\")",
         "render_zip(zip_file)\n",
         "# Step 5. Upload to LMS with a final grade.\n",
         "test_opal@academic_grading = TRUE",
