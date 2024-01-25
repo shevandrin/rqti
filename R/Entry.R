03981b099d616213debe8f61e50937bf2de9f5a0
Andrey Shevandrin <shevandrin@gmail.com>
2024-01-25 14:20:06 +0100

    refactor: suppress message "Overal points is recalculated as a sum of score of gaps" issue #143

--- R/Entry.R
+++ R/Entry.R
@@ -42,9 +42,6 @@ setMethod("initialize", "Entry", function(.Object, ...) {
     content_obj <- Map(getResponse, .Object@content)
     content_obj <- Filter(Negate(is.null), content_obj)
     points <- sum(sapply(content_obj, function(x) x@points))
-    if (.Object@points != points) {
-        message("Overal points is recalculated as a sum of score of gaps")
-    }
     .Object@points <- points
 
     # check identifiers
