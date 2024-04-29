be4b2e674d419d46e8fcc5ac760f0e5b069570c6
Andrey Shevandrin <shevandrin@gmail.com>
2024-04-29 10:09:25 +0200

    refactro: add roles to the message about contributors, that are found during test creation issue #183

--- R/section_builder.R
+++ R/section_builder.R
@@ -350,7 +350,7 @@ add_test_metadata <- function(object, params) {
         contr_ar <- lapply(contr, function(x) list(x@contributor, x@role))
         contr_unique <- contr[!duplicated(contr_ar)]
         if (length(contr_unique) != 0) {
-            msg <- paste(sapply(contr_unique, function(x) x@contributor),
+            msg <- paste(sapply(contr_unique, function(x) paste0(x@contributor, " (", x@role, ")")),
                          collapse = ", ")
             msg <- paste0("Authors are found in exercises and added as",
                           " contributors: ", msg, ".")
