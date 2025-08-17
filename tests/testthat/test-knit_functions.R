0025d576c3b23179a97761d75204711fe25d2551
Johannes Titz <johannes@titz.science>
2025-08-17 13:47:29 +0200

    refactor: improve waiting behavior and killing process for knit function tests

--- tests/testthat/test-knit_functions.R
+++ tests/testthat/test-knit_functions.R
@@ -1,23 +1,22 @@
-# test-servr.R
+# test-knit_functions
 library(testthat)
 library(callr)
 library(chromote)
 
 test_that("servr responds", {
-  testthat::skip_on_cran()
-  testthat::skip_on_covr()
+    testthat::skip_on_cran()
 
     # we need to test it in temp directory, otherwise we cannot copy files
     # which is necessary for rendering
     src <- fs::path_package("QTIJS", package = "rqti")
     dst <- file.path(tempdir(), "QTIJS")
     dir.create(dst, showWarnings = FALSE)
-    qtijs_path = dst#paste0(tempdir, "/QTIJS")
+    qtijs_path = dst
     # Copy *contents* of src into dst
     file.copy(
-      list.files(src, full.names = TRUE),
-      dst,
-      recursive = TRUE
+        list.files(src, full.names = TRUE),
+        dst,
+        recursive = TRUE
     )
 
     # start server in background process, otherwise it will block? see servr
@@ -31,31 +30,35 @@ test_that("servr responds", {
 
     }, supervise = TRUE, args = list(qtijs_path = qtijs_path))
 
-    # maybe not necessary because of supervise=T?
-    on.exit(p$kill)
-    Sys.sleep(3)  # give server time to start
+    # ensure cleanup even if later on.exit() calls are added
+    on.exit(p$kill(), add = TRUE)
+
+    # Sys.sleep(3)  # give server time to start
     url <- "http://127.0.0.1:4321/index.html"
 
     Sys.setenv(RQTI_URL="http://127.0.0.1:4321")
-    rqti::render_qtijs(fs::path_package("exercises", "sc1d.Rmd", package = "rqti"),
-                 qtijs_path = qtijs_path)
+    rqti::render_qtijs(fs::path_package("exercises", "sc1d.Rmd",
+                                        package = "rqti"),
+                       qtijs_path = qtijs_path)
 
     # now we can simply use chromote
     b <- ChromoteSession$new()
     on.exit(b$close(), add = TRUE)
 
     b$go_to(url)
-    # wait for qti to start
-    Sys.sleep(3)
-    # Use JavaScript to find element text containing "sc1d"
-    js <- "
-    (function() {
-      var el = document.querySelector('title');
-      return el ? el.textContent : null;
-    })();
-    "
 
-    found_text <- b$Runtime$evaluate(js)$result$value
+    # wait until the title is exactly "sc1d" (instead of fixed sleep)
+    js <- "document.title;"
+    found_text <- NULL
+    deadline <- Sys.time() + 10
+    repeat {
+        res <- b$Runtime$evaluate(js)
+        found_text <- res$result$value
+        if (identical(found_text, "sc1d")) break
+        if (Sys.time() > deadline) break
+        Sys.sleep(0.25)
+    }
+
     expect_equal(found_text, "sc1d")
     p$kill()
 })
